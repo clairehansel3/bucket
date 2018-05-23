32e18a6a24c1fb57bde4114aa7a8dfc560d389d2
Claire Hansel <clairehansel3@gmail.com>
2018-05-22 19:16:25 -0700

    changed symtable

--- code/symtable.cxx
+++ code/symtable.cxx
@@ -1,21 +1,18 @@
 #include "symtable.hxx"
 #include <boost/algorithm/string/predicate.hpp>
-#include <cassert>
-#include <cstddef>
+#include <iostream>
 #include <llvm/IR/DerivedTypes.h>
 #include <stdexcept>
 #include <utility>
 
-#include <iostream>
-
 namespace sym {
 
-std::string_view Entry::name() const noexcept {
-  return m_name;
+std::string_view Entry::fullname() const noexcept {
+  return m_fullname;
 }
 
-Entry::Entry(std::string&& name) noexcept
-: m_name{std::move(name)} {}
+Entry::Entry(std::string&& fullname) noexcept
+: m_fullname{std::move(fullname)} {}
 
 llvm::AllocaInst* Variable::value() const noexcept {
   return m_value;
@@ -25,19 +22,12 @@ void Variable::setValue(llvm::AllocaInst* value) noexcept {
   m_value = value;
 }
 
-Class* Variable::cls() const noexcept {
-  return m_cls;
+Class* Variable::classof() const noexcept {
+  return m_classof;
 }
 
-Variable::Variable(std::string&& name, Class* cls) noexcept
-: Entry{std::move(name)}, m_cls{cls} {}
-
-Class* Field::cls() const noexcept {
-  return m_cls;
-}
-
-Field::Field(std::string&& name, Class* cls) noexcept
-: Entry{std::move(name)}, m_cls{cls} {}
+Variable::Variable(std::string&& fullname, Class* classof) noexcept
+: Entry{std::move(fullname)}, m_value{nullptr}, m_classof{classof} {}
 
 llvm::Function* Method::function() const noexcept {
   return m_function;
@@ -55,69 +45,67 @@ Class* Method::returnClass() const noexcept {
   return m_return_class;
 }
 
-Method::Method(std::string&& name, std::vector<Class*>&& argument_classes, Class* return_class) noexcept
-: Entry{std::move(name)}, m_function{nullptr}, m_argument_classes{std::move(argument_classes)}, m_return_class{return_class} {}
+Method::Method(std::string&& fullname, std::vector<Class*>&& argument_classes, Class* return_class) noexcept
+: Entry{std::move(fullname)}, m_function{nullptr}, m_argument_classes{std::move(argument_classes)}, m_return_class{return_class} {}
+
+Class* Field::classof() const noexcept {
+  return m_classof;
+}
+
+Field::Field(std::string&& fullname, Class* classof) noexcept
+: Entry{std::move(fullname)}, m_classof{classof} {}
 
 llvm::Type* Class::type() const noexcept {
   return m_type;
 }
 
-Class* Class::pointer() {
-  if (!m_pointer) {
-    std::string new_name{name()};
-    new_name += '*';
-    m_pointer = std::unique_ptr<Class>(new Class{std::move(new_name), type()->getPointerTo()});
-  }
-  return m_pointer.get();
-}
+Class::Class(std::string&& fullname, llvm::Type* type) noexcept
+: Entry{std::move(fullname)}, m_type{type} {}
 
-Class::Class(std::string&& name, llvm::Type* type) noexcept
-: Entry{std::move(name)}, m_type{type} {}
-
-void Composite::setType(llvm::Type* type) noexcept {
+void CompositeClass::setType(llvm::Type* type) noexcept {
+  assert(type);
   m_type = type;
 }
 
-std::vector<Field*>& Composite::fields() noexcept {
+std::vector<Field*>& CompositeClass::fields() noexcept {
   return m_fields;
 }
 
-Composite::Composite(std::string&& name) noexcept
-: Class{std::move(name), nullptr} {}
+CompositeClass::CompositeClass(std::string&& fullname) noexcept
+: Class{std::move(fullname), nullptr} {}
 
-SymbolTable::SymbolTable() : m_scope{"."} {}
+SymbolTable::SymbolTable() : m_active_scope{"."} {}
 
-Entry* SymbolTable::lookup(std::string_view name) const {
-  //std::cout << "looking up name " << name << " in scope " << m_scope << std::endl;
-  //for (auto& item : m_table)
-  //  std::cout << item.first << ": " << typeid(*item.second).name() << std::endl;
-  assert(name.find('.') == std::string_view::npos);
-  std::string lookup_name{m_scope};
-  lookup_name += name;
-  while (true) {
-    auto iter = m_table.find(lookup_name);
-    if (iter != m_table.end())
-      return iter->second;
-    auto name_index = lookup_name.find_last_of('.');
-    if (name_index == 0)
-      return nullptr;
-    auto scope_index = lookup_name.find_last_of('.', name_index - 1);
-    auto new_length = lookup_name.size() - (name_index - scope_index);
-    for (auto i = scope_index + 1; i != new_length; ++i)
-      lookup_name[i] = lookup_name[name_index + (i - scope_index)];
-    lookup_name.erase(new_length);
-  }
+SymbolTable::iterator SymbolTable::begin() {
+  return iterator(m_entry_map.begin());
 }
 
-Entry* SymbolTable::lookup(std::string_view scope, std::string_view name) const {
-  assert(name.find('.') == std::string_view::npos);
+SymbolTable::iterator SymbolTable::end() {
+  return iterator(m_entry_map.end());
+}
+
+Entry* SymbolTable::lookup(std::string_view shortname) const {
+  std::string_view scope{m_active_scope.data(), m_active_scope.size() - 1};
+  return lookupInScope(scope, shortname);
+}
+
+Entry* SymbolTable::lookupKnown(std::string_view shortname) const {
+  std::string lookup_name{m_active_scope};
+  lookup_name += shortname;
+  auto iter = m_entry_map.find(lookup_name);
+  assert(iter != m_entry_map.end());
+  return iter->second.get();
+}
+
+Entry* SymbolTable::lookupInScope(std::string_view scope, std::string_view shortname) const {
+  assert(shortname.find('.') == std::string_view::npos);
   std::string lookup_name{scope};
   lookup_name += '.';
-  lookup_name += name;
+  lookup_name += shortname;
   while (true) {
-    auto iter = m_table.find(lookup_name);
-    if (iter != m_table.end())
-      return iter->second;
+    auto iter = m_entry_map.find(lookup_name);
+    if (iter != m_entry_map.end())
+      return iter->second.get();
     auto name_index = lookup_name.find_last_of('.');
     if (name_index == 0)
       return nullptr;
@@ -129,147 +117,139 @@ Entry* SymbolTable::lookup(std::string_view scope, std::string_view name) const 
   }
 }
 
-Composite* SymbolTable::getParent(Entry* entry) const {
-  auto index = entry->name().find_last_of('.');
+Entry* SymbolTable::lookupExact(std::string_view fullname) const {
+  assert(fullname.length() != 0 && fullname[0] == '.');
+  auto iter = m_entry_map.find(fullname);
+  return iter == m_entry_map.end() ? nullptr : iter->second.get();
+}
+
+CompositeClass* SymbolTable::getParentClass(Entry* entry) const {
+  auto index = entry->fullname().find_last_of('.');
   assert(index != std::string_view::npos && index != 0);
-  auto parent_name = entry->name().substr(0, index);
-  auto iter = m_table.find(parent_name);
-  assert(iter != m_table.end());
-  auto result = dynamic_cast<Composite*>(iter->second);
+  auto parent_name = entry->fullname().substr(0, index);
+  auto iter = m_entry_map.find(parent_name);
+  assert(iter != m_entry_map.end());
+  auto result = sym_cast<CompositeClass*>(iter->second.get());
   assert(result);
   return result;
 }
 
-void SymbolTable::pushScope(std::string_view name) {
-  assert(name.find('.') == std::string_view::npos);
-  m_scope += name;
-  m_scope += '.';
+Class* SymbolTable::getReferenceClass(Class* cls) {
+  std::string reference_class_name{cls->fullname()};
+  reference_class_name += '*';
+  if (auto reference_class_pointer = lookupExact(reference_class_name)) {
+    assert(sym_cast<CompositeClass*>(reference_class_pointer));
+    return static_cast<Class*>(reference_class_pointer);
+  }
+  auto reference_class_unique_pointer = std::unique_ptr<Class>(new Class{std::move(reference_class_name), cls->type()->getPointerTo()});
+  auto reference_class_pointer = reference_class_unique_pointer.get();
+  assert(m_entry_map.find(reference_class_pointer->fullname()) == m_entry_map.end());
+  m_entry_map[reference_class_pointer->fullname()] = std::move(reference_class_unique_pointer);
+  return reference_class_pointer;
+}
+
+
+void SymbolTable::pushScope(std::string_view scopename) {
+  assert(scopename.find('.') == std::string_view::npos);
+  m_active_scope += scopename;
+  m_active_scope += '.';
 }
 
 void SymbolTable::popScope() {
-  assert(m_scope != ".");
-  m_scope.erase(m_scope.find_last_of('.', m_scope.size() - 2) + 1);
+  assert(m_active_scope != ".");
+  m_active_scope.erase(m_active_scope.find_last_of('.', m_active_scope.size() - 2) + 1);
 }
 
 void SymbolTable::pushAnonymousScope() {
-  m_scope += '.';
+  m_active_scope += '.';
 }
 
 void SymbolTable::popAnonymousScope() {
-  std::size_t i = 0;
-  while (i != m_entries.size()) {
-    if (boost::starts_with(m_entries[i]->name(), m_scope)) {
-      m_table.erase(m_table.find(m_entries[i]->name()));
-      m_entries.erase(m_entries.begin() + static_cast<std::ptrdiff_t>(i));
-    } else {
-      ++i;
-    }
-  }
+  std::vector<Entry*> entries_to_delete;
+  for (auto entry : *this)
+    if (boost::starts_with(entry->fullname(), m_active_scope))
+      entries_to_delete.push_back(entry);
+  for (auto entry : entries_to_delete)
+    m_entry_map.erase(m_entry_map.find(entry->fullname()));
   popScope();
 }
 
-Variable* SymbolTable::createVariable(std::string_view name, Class* cls) {
-  assert(name.find('.') == std::string_view::npos);
-  std::string full_name{m_scope};
-  full_name += name;
-  std::unique_ptr<Variable> uptr{new Variable{std::move(full_name), cls}};
-  auto ptr = uptr.get();
-  m_entries.push_back(std::move(uptr));
-  if (m_table[ptr->name()])
+Variable* SymbolTable::createVariable(std::string_view shortname, Class* classof) {
+  assert(shortname.find('.') == std::string_view::npos);
+  std::string fullname{m_active_scope};
+  fullname += shortname;
+  auto variable_unique_pointer = std::unique_ptr<Variable>(new Variable{std::move(fullname), classof});
+  auto variable_pointer = variable_unique_pointer.get();
+  if (m_entry_map.find(variable_pointer->fullname()) != m_entry_map.end())
     throw std::runtime_error("name already taken");
-  m_table[ptr->name()] = ptr;
-  return ptr;
+  m_entry_map[variable_pointer->fullname()] = std::move(variable_unique_pointer);
+  return variable_pointer;
 }
 
-Field* SymbolTable::createField(std::string_view name, Class* cls) {
-  assert(name.find('.') == std::string_view::npos);
-  std::string full_name{m_scope};
-  full_name += name;
-  std::unique_ptr<Field> uptr{new Field{std::move(full_name), cls}};
-  auto ptr = uptr.get();
-  m_entries.push_back(std::move(uptr));
-  if (m_table[ptr->name()])
+Method* SymbolTable::createMethod(std::string_view shortname, std::vector<Class*>&& argument_classes, Class* return_class) {
+  assert(shortname.find('.') == std::string_view::npos);
+  std::string fullname{m_active_scope};
+  fullname += shortname;
+  auto method_unique_pointer = std::unique_ptr<Method>(new Method{std::move(fullname), std::move(argument_classes), return_class});
+  auto method_pointer = method_unique_pointer.get();
+  if (m_entry_map.find(method_pointer->fullname()) != m_entry_map.end())
     throw std::runtime_error("name already taken");
-  m_table[ptr->name()] = ptr;
-  return ptr;
+  m_entry_map[method_pointer->fullname()] = std::move(method_unique_pointer);
+  return method_pointer;
 }
 
-Method* SymbolTable::createMethod(std::string_view name, std::vector<Class*>&& argument_classes, Class* return_class) {
-  assert(name.find('.') == std::string_view::npos);
-  std::string full_name{m_scope};
-  full_name += name;
-  std::unique_ptr<Method> uptr{new Method{std::move(full_name), std::move(argument_classes), return_class}};
-  auto ptr = uptr.get();
-  m_entries.push_back(std::move(uptr));
-  if (m_table[ptr->name()])
+Field* SymbolTable::createField(std::string_view shortname, Class* classof) {
+  assert(shortname.find('.') == std::string_view::npos);
+  std::string fullname{m_active_scope};
+  fullname += shortname;
+  auto field_unique_pointer = std::unique_ptr<Field>(new Field{std::move(fullname), classof});
+  auto field_pointer = field_unique_pointer.get();
+  if (m_entry_map.find(field_pointer->fullname()) != m_entry_map.end())
     throw std::runtime_error("name already taken");
-  m_table[ptr->name()] = ptr;
-  return ptr;
+  m_entry_map[field_pointer->fullname()] = std::move(field_unique_pointer);
+  return field_pointer;
 }
 
-Method* SymbolTable::createMethod(std::string scope, std::string_view name, std::vector<Class*>&& argument_classes, Class* return_class) {
-  assert(name.find('.') == std::string_view::npos);
-  std::string full_name{m_scope};
-  full_name += name;
-  std::unique_ptr<Method> uptr{new Method{std::move(full_name), std::move(argument_classes), return_class}};
-  auto ptr = uptr.get();
-  m_entries.push_back(std::move(uptr));
-  if (m_table[ptr->name()])
+Class* SymbolTable::createClass(std::string_view shortname, llvm::Type* type) {
+  assert(shortname.find('.') == std::string_view::npos);
+  std::string fullname{m_active_scope};
+  fullname += shortname;
+  auto class_unique_pointer = std::unique_ptr<Class>(new Class{std::move(fullname), type});
+  auto class_pointer = class_unique_pointer.get();
+  if (m_entry_map.find(class_pointer->fullname()) != m_entry_map.end())
     throw std::runtime_error("name already taken");
-  m_table[ptr->name()] = ptr;
-  return ptr;
+  m_entry_map[class_pointer->fullname()] = std::move(class_unique_pointer);
+  return class_pointer;
 }
 
-Class* SymbolTable::createClass(std::string_view name, llvm::Type* type) {
-  assert(name.find('.') == std::string_view::npos);
-  std::string full_name{m_scope};
-  full_name += name;
-  std::unique_ptr<Class> uptr{new Class{std::move(full_name), type}};
-  auto ptr = uptr.get();
-  m_entries.push_back(std::move(uptr));
-  if (m_table[ptr->name()])
+CompositeClass* SymbolTable::createCompositeClass(std::string_view shortname) {
+  assert(shortname.find('.') == std::string_view::npos);
+  std::string fullname{m_active_scope};
+  fullname += shortname;
+  auto composite_class_unique_pointer = std::unique_ptr<CompositeClass>(new CompositeClass{std::move(fullname)});
+  auto composite_class_pointer = composite_class_unique_pointer.get();
+  if (m_entry_map.find(composite_class_pointer->fullname()) != m_entry_map.end())
     throw std::runtime_error("name already taken");
-  m_table[ptr->name()] = ptr;
-  return ptr;
+  m_entry_map[composite_class_pointer->fullname()] = std::move(composite_class_unique_pointer);
+  return composite_class_pointer;
 }
 
-Composite* SymbolTable::createComposite(std::string_view name) {
-  assert(name.find('.') == std::string_view::npos);
-  std::string full_name{m_scope};
-  full_name += name;
-  std::unique_ptr<Composite> uptr{new Composite{std::move(full_name)}};
-  auto ptr = uptr.get();
-  m_entries.push_back(std::move(uptr));
-  if (m_table[ptr->name()])
-    throw std::runtime_error("name already taken");
-  m_table[ptr->name()] = ptr;
-  return ptr;
-}
-
-void SymbolTable::forEachEntry(const std::function<void(Entry*)>& function) {
-  for (auto& entry : m_entries)
-    function(entry.get());
-}
-
-void SymbolTable::showSymbols() {
-  auto show = [](Entry* entry){
-    if (!entry)
-      return "nullptr";
-    else if (dynamic_cast<Variable*>(entry))
+void SymbolTable::print() {
+  auto entryKind = [](Entry* entry) {
+    assert(entry);
+    if (sym_cast<Variable*>(entry))
       return "variable";
-    else if (dynamic_cast<Field*>(entry))
-      return "field";
-    else if (dynamic_cast<Method*>(entry))
+    if (sym_cast<Method*>(entry))
       return "method";
-    else if (dynamic_cast<Composite*>(entry))
-      return "composite";
-    else {
-      assert(dynamic_cast<Class*>(entry));
-      return "class";
-    }
+    if (sym_cast<Field*>(entry))
+      return "field";
+    if (sym_cast<CompositeClass*>(entry))
+      return "composite class";
+    assert(sym_cast<Class*>(entry));
+    return "class";
   };
-  for (auto& [k, v] : m_table)
-    std::cerr << k << ": " << show(v) << std::endl;
+  for (auto entry : *this)
+    std::cout << entry->fullname() << " | " << entryKind(entry) << '\n';
 }
 
 }
--- code/symtable.hxx
+++ code/symtable.hxx
@@ -1,8 +1,14 @@
-#ifndef BUCKET_SYMBOL_TABLE_HXX
-#define BUCKET_SYMBOL_TABLE_HXX
+#ifndef BUCKET_SYMTABLE_HXX
+#define BUCKET_SYMTABLE_HXX
 
-#include <boost/core/noncopyable.hpp>
-#include <functional>
+#ifdef BUCKET_DEBUG
+  #define BUCKET_IF_DEBUG(X) X
+#else
+  #define BUCKET_IF_DEBUG(X)
+#endif
+
+#include <boost/iterator/iterator_adaptor.hpp>
+#include <cassert>
 #include <memory>
 #include <string>
 #include <string_view>
@@ -11,9 +17,9 @@
 
 namespace llvm {
 
+class AllocaInst;
 class Function;
 class Type;
-class AllocaInst;
 
 }
 
@@ -21,19 +27,35 @@ namespace sym {
 
 class Entry;
 class Variable;
-class Field;
 class Method;
+class Field;
 class Class;
-class Composite;
+class CompositeClass;
 
-class Entry : private boost::noncopyable {
+class Visitor {
 public:
+  virtual ~Visitor() = default;
+  virtual void visit(Entry*)          {assert(false);}
+  virtual void visit(Variable*)       {assert(false);}
+  virtual void visit(Method*)         {assert(false);}
+  virtual void visit(Field*)          {assert(false);}
+  virtual void visit(Class*)          {assert(false);}
+  virtual void visit(CompositeClass*) {assert(false);}
+};
+
+class Entry {
+public:
+  Entry(const Entry&) = delete;
+  Entry(Entry&&) = delete;
+  Entry& operator=(const Entry&) = delete;
+  Entry& operator=(Entry&&) = delete;
   virtual ~Entry() = default;
-  std::string_view name() const noexcept;
+  std::string_view fullname() const noexcept;
+  virtual void receive(Visitor* visitor) {visitor->visit(this);}
 protected:
-  Entry(std::string&& name) noexcept;
+  explicit Entry(std::string&& fullname) noexcept;
 private:
-  const std::string m_name;
+  const std::string m_fullname;
 };
 
 class Variable final : public Entry {
@@ -41,20 +63,13 @@ friend class SymbolTable;
 public:
   llvm::AllocaInst* value() const noexcept;
   void setValue(llvm::AllocaInst* value) noexcept;
-  Class* cls() const noexcept;
+  Class* classof() const noexcept;
+  void receive(Visitor* visitor) override {visitor->visit(this);}
 private:
+  BUCKET_IF_DEBUG(bool m_value_is_set;)
   llvm::AllocaInst* m_value;
-  Class* const m_cls;
-  Variable(std::string&& name, Class* cls) noexcept;
-};
-
-class Field final : public Entry {
-friend class SymbolTable;
-public:
-  Class* cls() const noexcept;
-private:
-  Class* const m_cls;
-  Field(std::string&& name, Class* cls) noexcept;
+  Class* const m_classof;
+  Variable(std::string&& fullname, Class* classof) noexcept;
 };
 
 class Method final : public Entry {
@@ -64,56 +79,123 @@ public:
   void setFunction(llvm::Function* function) noexcept;
   const std::vector<Class*>& argumentClasses() const noexcept;
   Class* returnClass() const noexcept;
+  void receive(Visitor* visitor) override {visitor->visit(this);}
 private:
   llvm::Function* m_function;
   const std::vector<Class*> m_argument_classes;
   Class* const m_return_class;
-  Method(std::string&& name, std::vector<Class*>&& argument_classes, Class* return_class) noexcept;
+  Method(std::string&& fullname, std::vector<Class*>&& argument_classes, Class* return_class) noexcept;
+};
+
+class Field final : public Entry {
+friend class SymbolTable;
+public:
+  Class* classof() const noexcept;
+private:
+  Class* const m_classof;
+  Field(std::string&& fullname, Class* classof) noexcept;
 };
 
 class Class : public Entry {
 friend class SymbolTable;
 public:
   llvm::Type* type() const noexcept;
-  Class* pointer();
+  void receive(Visitor* visitor) override {visitor->visit(this);}
 protected:
+  BUCKET_IF_DEBUG(bool m_type_is_set;)
   llvm::Type* m_type;
-  std::unique_ptr<Class> m_pointer;
-  Class(std::string&& name, llvm::Type* type) noexcept;
+  Class(std::string&& fullname, llvm::Type* type) noexcept;
 };
 
-class Composite final : public Class {
+class CompositeClass final : public Class {
 friend class SymbolTable;
 public:
   void setType(llvm::Type* type) noexcept;
   std::vector<Field*>& fields() noexcept;
+  void receive(Visitor* visitor) override {visitor->visit(this);}
 private:
   std::vector<Field*> m_fields;
-  Composite(std::string&& name) noexcept;
+  CompositeClass(std::string&& fullname) noexcept;
 };
 
+namespace details {
+
+template <typename To>
+class Caster final : public Visitor {
+public:
+  To* result;
+private:
+  void visit(Entry* pointer)          {cast(pointer);}
+  void visit(Variable* pointer)       {cast(pointer);}
+  void visit(Method* pointer)         {cast(pointer);}
+  void visit(Field* pointer)          {cast(pointer);}
+  void visit(Class* pointer)          {cast(pointer);}
+  void visit(CompositeClass* pointer) {cast(pointer);}
+  template <typename From>
+  void cast([[maybe_unused]] From* pointer) {
+    if constexpr (std::is_base_of_v<To, From>)
+      result = static_cast<To*>(pointer);
+    else
+      result = nullptr;
+  }
+};
+
+}
+
+template <typename ToPointer, typename FromPointer>
+ToPointer sym_cast(FromPointer pointer) {
+  assert(pointer);
+  static_assert(std::is_pointer_v<ToPointer>);
+  static_assert(std::is_pointer_v<FromPointer>);
+  using To   = std::remove_pointer_t<ToPointer>;
+  using From = std::remove_pointer_t<FromPointer>;
+  static_assert(std::is_base_of_v<Entry, To>);
+  static_assert(std::is_base_of_v<Entry, From>);
+  details::Caster<To> caster;
+  const_cast<std::add_pointer_t<std::remove_const_t<From>>>(pointer)->receive(&caster);
+  assert(caster.result == dynamic_cast<ToPointer>(pointer));
+  return caster.result;
+}
+
 class SymbolTable {
+private:
+  class iterator : public boost::iterator_adaptor<
+    iterator,
+    std::unordered_map<std::string_view, std::unique_ptr<Entry>>::iterator,
+    Entry*,
+    boost::forward_traversal_tag,
+    Entry*
+  > {
+  public:
+    iterator() : iterator::iterator_adaptor_() {}
+    iterator(const iterator::iterator_adaptor_::base_type& p) : iterator::iterator_adaptor_(p) {}
+  private:
+    friend class boost::iterator_core_access;
+    Entry* dereference() const {return this->base()->second.get();}
+  };
 public:
   SymbolTable();
-  Entry* lookup(std::string_view name) const;
-  Entry* lookup(std::string_view scope, std::string_view name) const;
-  Composite* getParent(Entry* entry) const;
-  void pushScope(std::string_view name);
+  iterator begin();
+  iterator end();
+  Entry* lookup(std::string_view shortname) const;
+  Entry* lookupKnown(std::string_view shortname) const;
+  Entry* lookupInScope(std::string_view scope, std::string_view shortname) const;
+  Entry* lookupExact(std::string_view fullname) const;
+  CompositeClass* getParentClass(Entry* entry) const;
+  Class* getReferenceClass(Class* cls);
+  void pushScope(std::string_view scopename);
   void popScope();
   void pushAnonymousScope();
   void popAnonymousScope();
-  Variable* createVariable(std::string_view name, Class* cls);
-  Field* createField(std::string_view name, Class* cls);
-  Method* createMethod(std::string_view name, std::vector<Class*>&& argument_classes, Class* return_class);
-  Method* createMethod(std::string scope, std::string_view name, std::vector<Class*>&& argument_classes, Class* return_class);
-  Class* createClass(std::string_view name, llvm::Type* type);
-  Composite* createComposite(std::string_view name);
-  void forEachEntry(const std::function<void(Entry*)>& function);
-  void showSymbols();
+  Variable* createVariable(std::string_view shortname, Class* classof);
+  Method* createMethod(std::string_view shortname, std::vector<Class*>&& argument_classes, Class* return_class);
+  Field* createField(std::string_view shortname, Class* classof);
+  Class* createClass(std::string_view shortname, llvm::Type* type);
+  CompositeClass* createCompositeClass(std::string_view shortname);
+  void print();
 private:
-  std::unordered_map<std::string_view, Entry*> m_table;
-  std::string m_scope;
-  std::vector<std::unique_ptr<Entry>> m_entries;
+  std::unordered_map<std::string_view, std::unique_ptr<Entry>> m_entry_map;
+  std::string m_active_scope;
 };
 
 }
